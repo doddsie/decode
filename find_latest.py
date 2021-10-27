f35c187e33f16408d1dd79daeef8fa07ac22e436
David Dodd <thedoddsie@gmail.com>
2021-10-26 21:09:20 -0700

    Create find_latest.py

---
+++ find_latest.py
@@ -0,0 +1,17 @@
+import os, os.path
+  
+  
+def find_latest(start_dir):
+      latest_time = 0
+      latest_file = None
+      for root, subdirs, files in os.walk(start_dir):
+          for name in files:
+              modified_time = os.path.getmtime(os.path.join(root, name))
+              if (modified_time > latest_time):
+                  latest_file = os.path.join(root, name)
+      return os.path.abspath(latest_file)
+
+      
+      
+      
+      
